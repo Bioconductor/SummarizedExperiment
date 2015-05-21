c1bdb30f3da9900eb1045aa76aeed3dd5ccf10cc
Herve Pages <hpages@fhcrc.org>
2015-05-21 19:25:33 +0000

    Start adding documentation and scripts for finding and updating serialized objects located in a directory hierarchy.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SummarizedExperiment@104051 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/prepare_list_of_rda_files_to_update.R
@@ -0,0 +1,80 @@
+### =========================================================================
+### prepare_list_of_rda_files_to_update.R
+### -------------------------------------------------------------------------
+###
+### This script performs STEP 2 of the "Find and update objects" procedure
+### described in the 'Find_and_update_objects.txt' file located in the same
+### folder.
+###
+### Before you run this script, make sure you performed STEP 1, that is, you
+### need to generate input file 'rda_files'. This can be achieved with
+### something like:
+###
+###   cd <dir/you/want/to/scan>
+###   find . -type d -name '.svn' -prune -o -type f -print | \
+###       grep -Ei '\.(rda|RData)$' >rda_files
+###
+### See Find_and_update_objects.txt for more information.
+###
+### Then to run STEP 2 in "batch" mode:
+###
+###   cd <dir/you/want/to/scan>  # the 'rda_files' file should be here
+###   R CMD BATCH prepare_list_of_rda_files_to_update.R \
+###              >prepare_list_of_rda_files_to_update.log 2>&1 &
+###
+### This can take a few hours to complete...
+###
+### The output of STEP 2 is a file created in the current directory and named
+### 'rda_files_to_update'. It has 1 line per object to update and 4 fields
+### separated by tabs:
+###   1. Path to rda file (as found in input file 'rda_files').
+###   2. Name of object to update.
+###   3. Class of object to update.
+###   4. Package where class of object to update is defined.
+###
+
+infile <- "rda_files"
+outfile <- "rda_files_to_update"
+
+rda_files <- read.table(infile, stringsAsFactors=FALSE)[[1L]]
+file.remove(outfile)
+
+library(SummarizedExperiment)
+
+installAndLoadPkg <- function(package)
+{
+    if (suppressWarnings(suppressPackageStartupMessages(
+            require(package, character.only=TRUE, quietly=TRUE)
+        )))
+        return()
+    suppressWarnings(suppressPackageStartupMessages(
+        library(BiocInstaller, quietly=TRUE)
+    ))
+    biocLite(pkg)
+    suppressWarnings(suppressPackageStartupMessages(
+        library(pkg, character.only=TRUE, quietly=TRUE)
+    ))
+}
+
+for (i in seq_along(rda_files)) {
+    rda_path <- rda_files[[i]]
+
+    cat("[", i , "/", length(rda_files), "] Loading ",
+        rda_path, " ... ", sep="")
+    envir <- new.env(parent=emptyenv())
+    load(rda_path, envir=envir)
+    cat("OK\n")
+
+    for (objname in names(envir)) {
+        obj <- get(objname, envir=envir)
+        class_pkg <- attr(class(obj), "package")
+        if (!is.null(class_pkg))
+            installAndLoadPkg(class_pkg)
+        if (is(obj, "SummarizedExperiment")
+         || is(obj, "RangedSummarizedExperiment")) {
+            cat(rda_path, "\t", objname, "\t", class(obj), "\t",
+                class_pkg, "\n", sep="", file=outfile, append=TRUE)
+        }
+    }
+}
+
