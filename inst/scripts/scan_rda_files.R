54bbea8d59cf4411ea4c95a11e44813b1f62651e
Herve Pages <hpages@fhcrc.org>
2015-05-22 08:18:01 +0000

    more work on the "Find and update objects" procedure
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SummarizedExperiment@104098 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/scan_rda_files.R
+++ inst/scripts/scan_rda_files.R
@@ -51,6 +51,10 @@ scanRdaFiles <- function(rda_files, outfile="")
             obj <- get(objname, envir=envir)
             obj_class <- class(obj)
             obj_class_pkg <- attr(obj_class, "package")
+
+            ## Fix 'obj_class' and 'obj_class_pkg' (they both need to be
+            ## character vectors of length 1 before we pass them to paste()).
+            obj_class <- obj_class[[1L]]
             if (is.null(obj_class_pkg))
                 obj_class_pkg <- "."
             outline <- paste(rda_path, objname, obj_class, obj_class_pkg,
