b21340b3d92e7de338658b5681d28f9a6edb91ed
Herve Pages <hpages@fhcrc.org>
2015-05-22 22:30:05 +0000

    more work on the "Find and update objects" procedure
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SummarizedExperiment@104127 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/prepare_list_of_rda_files_to_update.R
+++ inst/scripts/prepare_list_of_rda_files_to_update.R
@@ -40,18 +40,26 @@ prepareListOfRdaFilesToUpdate <- function(rda_objects, outfile="")
     ## Install missing packages.
     installed_pkgs <- rownames(installed.packages())
     missing_pkgs <- setdiff(pkgs, installed_pkgs)
-    if (length(missing_pkgs) != 0L)
+    if (length(missing_pkgs) != 0L) {
         biocLite(missing_pkgs)
+        installed_pkgs <- rownames(installed.packages())
+        missing_pkgs <- setdiff(pkgs, installed_pkgs)
+        if (length(missing_pkgs) != 0L) {
+            ## Some packages could not be installed.
+            pkgs <- intersect(pkgs, installed_pkgs)
+            pkg2class <- pkg2class[pkgs]
+        }
+    }
 
     ## Load packages where all object classes are defined.
     ## Unfortunately, this sometimes hits the maximal number of DLLs that can
     ## be loaded ("maximal number of DLLs reached..." error).
     #for (i in seq_along(pkgs)) {
-    #    package <- pkgs[[i]]
-    #    cat("[", i , "/", length(pkgs), "] Loading ", package, " ... ",
+    #    pkg <- pkgs[[i]]
+    #    cat("[", i , "/", length(pkgs), "] Loading ", pkg, " ... ",
     #        sep="")
     #    suppressWarnings(suppressPackageStartupMessages(
-    #        require(package, character.only=TRUE, quietly=TRUE)
+    #        library(pkg, character.only=TRUE, quietly=TRUE)
     #    ))
     #    cat("OK\n")
     #}
@@ -59,18 +67,18 @@ prepareListOfRdaFilesToUpdate <- function(rda_objects, outfile="")
     ## We use subprocesses to work around the "maximal number of DLLs
     ## reached..." error.
     for (i in seq_along(pkgs)) {
-        package <- pkgs[[i]]
+        pkg <- pkgs[[i]]
         cat("[", i , "/", length(pkgs), "] Check classes defined in package ",
-            package, " ... ", sep="")
-        classes <- paste0("\"", pkg2class[[package]], "\"")
+            pkg, " ... ", sep="")
+        classes <- paste0("\"", pkg2class[[pkg]], "\"")
         classes <- paste0("c(", paste(classes, collapse=", "), ")")
-        class_summary_file <- paste0(package, "_class_summary")
+        class_summary_file <- paste0(pkg, "_class_summary")
         input <- c(
                 "suppressWarnings(suppressPackageStartupMessages(",
         sprintf("    library(%s)", "SummarizedExperiment"),
                 "))",
                 "suppressWarnings(suppressPackageStartupMessages(",
-        sprintf("    library(%s)", package),
+        sprintf("    library(%s)", pkg),
                 "))",
         sprintf("ok <- sapply(%s, function(class) {", classes),
                 "    extends(class, \"SummarizedExperiment\") ||",
@@ -87,23 +95,28 @@ prepareListOfRdaFilesToUpdate <- function(rda_objects, outfile="")
     }
 
     ## Collect the class summary files.
-    class_summaries <- lapply(pkgs, function(package) {
-        class_summary_file <- paste0(package, "_class_summary")
-        if (!file.exists(class_summary_file))
-            return(NULL)
+    class_summaries <- lapply(pkgs, function(pkg) {
+        class_summary_file <- paste0(pkg, "_class_summary")
         read.table(class_summary_file, stringsAsFactors=FALSE)
     })
     class_summary <- do.call(rbind, class_summaries)
 
+    ## Sanity check.
+    class1 <- class_summary[ , "class"]
+    class2 <- unlist(pkg2class, use.names=FALSE)
+    stopifnot(identical(class1, class2))
+
     ## Write output to file.
-    class2ok <- class_summary[ , "ok"]
-    names(class2ok) <- class_summary[ , "class"]
+    class2ok <- setNames(class_summary[ , "ok"], class1)
     objclass <- rda_objects[ , "objclass"]
     ok <- class2ok[objclass]
     ok[is.na(ok)] <- FALSE
-    rda_files_to_update <- rda_objects[ok, ]
-    rownames(rda_files_to_update) <- NULL
-    write.table(rda_files_to_update, file=outfile, sep="\t")
+    rda_files_to_update <- rda_objects[ok, , drop=FALSE]
+    rda_files_to_update <- do.call(
+        paste,
+        c(as.list(rda_files_to_update), list(sep="\t"))
+    )
+    writeLines(rda_files_to_update, con=outfile)
 }
 
 rda_objects <- read.table(INFILE, stringsAsFactors=FALSE)
