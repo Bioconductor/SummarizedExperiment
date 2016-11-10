b9dc9c229fb1125ccbcd856306ba2386dff00fd0
Herve Pages <hpages@fhcrc.org>
2016-11-10 06:53:35 +0000

    replace 'force=TRUE' with 'pruning.mode="coarse"' in seqlevels() setter
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SummarizedExperiment@123845 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RangedSummarizedExperiment-class.R
+++ R/RangedSummarizedExperiment-class.R
@@ -478,13 +478,17 @@ setMethod("seqinfo", "RangedSummarizedExperiment",
 })
 
 setReplaceMethod("seqinfo", "RangedSummarizedExperiment",
-    function (x, new2old = NULL, force = FALSE, value)
+    function (x, new2old= NULL, force=FALSE,
+              pruning.mode=c("error", "coarse", "fine", "tidy"),
+              value)
 {
     if (!is(value, "Seqinfo")) 
         stop("the supplied 'seqinfo' must be a Seqinfo object")
     dangling_seqlevels <-
-        GenomeInfoDb:::getDanglingSeqlevels(x@rowRanges, new2old = new2old,
-                                            force = force, seqlevels(value))
+        GenomeInfoDb:::getDanglingSeqlevels(x@rowRanges, new2old=new2old,
+                                            force=force,
+                                            pruning.mode=pruning.mode,
+                                            seqlevels(value))
     if (length(dangling_seqlevels) != 0L) 
         x <- x[!(seqnames(x) %in% dangling_seqlevels)]
     x@rowRanges <-
