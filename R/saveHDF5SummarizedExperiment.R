19d2dfae2f481113298bbcc2dde9dd3c37fb68be
Herve Pages <hpages@fhcrc.org>
2017-02-09 08:20:22 +0000

    some additional tweaks to saveHDF5SummarizedExperiment()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SummarizedExperiment@126626 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/saveHDF5SummarizedExperiment.R
+++ R/saveHDF5SummarizedExperiment.R
@@ -3,26 +3,11 @@
 ### -------------------------------------------------------------------------
 
 
-### Save all the assays in HDF5 format, including in-memory assays.
-### Delayed assays with delayed operations on them are realized while they
-### are written to disk..
-saveHDF5SummarizedExperiment <- function(x, dir="myse", verbose=FALSE)
+.write_h5_assays <- function(assays, h5_file, verbose)
 {
-    if (!is(x, "SummarizedExperiment"))
-        stop("'x' must be a SummarizedExperiment object")
-    if (!isTRUEorFALSE(verbose))
-        stop("'verbose' must be TRUE or FALSE")
-    library(HDF5Array)  # for writeHDF5Array()
-    if (!isSingleString(dir))
-        stop(wmsg("'dir' must be a single string specifying the path ",
-                  "to the directory where to save the ", class(x),
-                  " object (the directory will be created)"))
-    if (!suppressWarnings(dir.create(dir)))
-        stop("cannot create dir \"", dir, "\"")
-    h5_file <- file.path(dir, "se.h5")
-    nassay <- length(assays(x))
+    nassay <- length(assays)
     for (i in seq_len(nassay)) {
-        a <- assay(x, i, withDimnames=FALSE)
+        a <- assays[[i]]
         h5_name <- sprintf("assay%03d", i)
         if (verbose)
             message("Start writing assay ", i, "/", nassay, " to '",
@@ -31,12 +16,52 @@ saveHDF5SummarizedExperiment <- function(x, dir="myse", verbose=FALSE)
         if (verbose)
             message("Finished writing assay ", i, "/", nassay, " to '",
                     h5_file, "'.")
-        a@seed@file <- basename(a@seed@file)
-        assay(x, i) <- a
+        assays[[i]] <- a
     }
+    assays
+}
+
+.shorten_assay_h5_paths <- function(assays)
+{
+    nassay <- length(assays)
+    for (i in seq_len(nassay)) {
+        a <- assays[[i]]
+        a@seed@file <- basename(a@seed@file)
+        assays[[i]] <- a
+    }
+    assays
+}
+
+### Save all the assays in HDF5 format, including in-memory assays.
+### Delayed assays with delayed operations on them are realized while they
+### are written to disk..
+saveHDF5SummarizedExperiment <- function(x, dir="my_h5_se", verbose=FALSE)
+{
+    if (!is(x, "SummarizedExperiment"))
+        stop("'x' must be a SummarizedExperiment object")
+    if (!isTRUEorFALSE(verbose))
+        stop("'verbose' must be TRUE or FALSE")
+    if (!isSingleString(dir))
+        stop(wmsg("'dir' must be a single string specifying the path ",
+                  "to the directory where to save the ", class(x),
+                  " object (the directory will be created)"))
+
+    ## Try library(HDF5Array) before creating directory 'dir'. That way if
+    ## HDF5Array is not installed then saveHDF5SummarizedExperiment() will
+    ## stop without having created an empty directory.
+    library(HDF5Array)  # for writeHDF5Array()
+    if (!suppressWarnings(dir.create(dir)))
+        stop("cannot create dir \"", dir, "\"")
+
+    h5_file <- file.path(dir, "assays.h5")
+    x@assays <- .write_h5_assays(x@assays, h5_file, verbose)
+
     rds_file <- file.path(dir, "se.rds")
+    ans <- x
+    x@assays <- .shorten_assay_h5_paths(x@assays)
     saveRDS(x, file=rds_file)
-    invisible(x)
+
+    invisible(ans)
 }
 
 .THE_EXPECTED_STUFF <- c(
@@ -50,14 +75,14 @@ saveHDF5SummarizedExperiment <- function(x, dir="myse", verbose=FALSE)
 
 ### Does a lot of checking and tries to fail graciously if the content
 ### of 'dir' doesn't look as expected.
-loadHDF5SummarizedExperiment <- function(dir="myse")
+loadHDF5SummarizedExperiment <- function(dir="my_h5_se")
 {
     library(rhdf5)  # for h5ls()
     library(HDF5Array)  # for the HDF5Array class
     if (!isSingleString(dir))
         stop(wmsg("'dir' must be a single string specifying the path ",
                   "to the directory containing ", .THE_EXPECTED_STUFF))
-    h5_file <- file.path(dir, "se.h5")
+    h5_file <- file.path(dir, "assays.h5")
     rds_file <- file.path(dir, "se.rds")
     if (!file.exists(h5_file) || !file.exists(rds_file))
         .stop_if_bad_dir(dir)
@@ -70,7 +95,7 @@ loadHDF5SummarizedExperiment <- function(dir="myse")
         .stop_if_bad_dir(dir)
     for (i in seq_along(assays(ans))) {
         a <- assay(ans, i, withDimnames=FALSE)
-        if (!is(a, "HDF5Array") || !identical(a@seed@file, "se.h5") ||
+        if (!is(a, "HDF5Array") || !identical(a@seed@file, "assays.h5") ||
             !(a@seed@name %in% h5_datasets))
             .stop_if_bad_dir(dir)
         a@seed@file <- file.path(dir, a@seed@file)
