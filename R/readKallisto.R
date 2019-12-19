0f5d66d4f4b181d92e584564ecfce1b27b09c203
LiNk-NY <marcel.ramosperez@roswellpark.org>
2019-12-19 14:16:51 -0500

    update error message

--- R/readKallisto.R
+++ R/readKallisto.R
@@ -168,7 +168,7 @@ readKallistoBootstrap <-
     }
     if (!missing(j) && is.numeric(j)) {
         if (any(j < 1L) || any(j > nboot))
-            stop("'j' must be > 0 and <=", nboot)
+            stop("'j' must be >= 1 and <=", nboot)
         j <- paste0("bs", as.integer(j) - 1L)
     }
 
