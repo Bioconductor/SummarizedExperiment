433bde0ae7460bb875da950fdc318f51d3be657a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-02-21 22:43:40 -0800

    Improved handling of unnamed assays in combineRows. Added documentation and basic tests.

--- R/combine-methods.R
+++ R/combine-methods.R
@@ -6,99 +6,117 @@ setMethod("combineRows", "SummarizedExperiment", function(x, y, ..., use.names=T
 
     # Combining the rowData.
     all.rd <- lapply(all.se, rowData) 
-    all.rd <- do.call(rbind, all.rd) # TODO: replace with combineRows for a DF.
+    tryCatch({
+        com.rd <- do.call(combineRows, all.rd)
+    }, error=function(e) {
+        stop(paste0("failed to combine rowData of SummarizedExperiment objects:\n  ", conditionMessage(e)))
+    })
 
     # Combining the colData. This constructs mappings of the columns for each
     # SE to the columns of the final object.
     all.cd <- lapply(all.se, colData)
+    tryCatch({
+        com.cd <- do.call(combineCols, c(all.cd, list(use.names=use.names)))
+    }, error=function(e) {
+        stop(paste0("failed to combine colData of SummarizedExperiment objects:\n  ", conditionMessage(e)))
+    })
 
     if (use.names) {
-        col.names <- lapply(all.se, colnames)
-        all.names <- Reduce(union, col.names)
-
+        # If combineCols succeeded, all SE's should have valid row names.
+        all.names <- rownames(com.cd)
         mappings <- vector("list", length(all.se))
         for (i in seq_along(mappings)) {
-            cur.names <- col.names[[i]]
-            if (is.null(cur.names) || anyDuplicated(cur.names)) {
-                stop(wmsg("each SummarizedExperiment must have non-NULL, ",
-                          "non-duplicated column names when 'use.names=TRUE'"))
-            }
-            mappings[[i]] <- match(all.names, cur.names)
+            mappings[[i]] <- match(all.names, rownames(all.cd[[i]]))
         }
-
-        for (i in seq_along(all.cd)) {
-            expanded <- all.cd[[i]][mappings[[i]],,drop=FALSE]
-            rownames(expanded) <- all.names
-            all.cd[[i]] <- expanded
-        }
-        all.cd <- do.call(cbind, all.cd) 
-
     } else {
-        nc <- unique(vapply(all.se, ncol, 0L))
-        if (length(nc)!=1) {
-            stop(wmsg("all SummarizedExperiment objects must have the same ",
-                      "number of columns when 'use.names=FALSE'"))
-        }
         mappings <- NULL
-
-        all.cd <- do.call(cbind, all.cd) 
     }
 
     # Assembling the SE.
     SummarizedExperiment(
         assays=.unsplit_assays(all.se, mappings, delayed=delayed),
-        colData=all.cd,
-        rowData=all.rd,
+        colData=com.cd,
+        rowData=com.rd,
         metadata=unlist(lapply(all.se, metadata), recursive=FALSE, use.names=FALSE)
     )
 })
 
 .unsplit_assays <- function(all.se, mappings, delayed) {
-    all.assays <- unique(unlist(lapply(all.se, assayNames)))
-    combined <- vector("list", length(all.assays))
-    names(combined) <- all.assays
-
-    for (a in all.assays) {
-        current <- vector("list", length(all.se))
-
-        for (s in seq_along(all.se)) {
-            cur.se <- all.se[[s]]
-            i <- mappings[[s]]
-
-            if (a %in% assayNames(cur.se)) {
-                mat <- assay(cur.se, a, withDimnames=FALSE)
-                if (delayed) {
-                    mat <- DelayedArray(mat)
-                }
-
-                # Doing something to handle the missing columns, if there are any.
-                if (!is.null(i)) {
-                    absent <- is.na(i)
-                    if (any(absent)) {
-                        i[absent] <- ncol(mat)+1L
-                        mat <- cbind(mat, ConstantArray(c(nrow(cur.se), 1L), value=NA))
-                    }
-                    mat <- mat[,i,drop=FALSE]
-                }
-            } else {
-                if (is.null(i)) {
-                    nc <- ncol(cur.se)
-                } else {
-                    nc <- length(i)
-                }
-
-                mat <- ConstantArray(c(nrow(cur.se), nc), value=NA)
-                if (!delayed) {
-                    mat <- as.matrix(mat)
-                }
-            }
-
-            current[[s]] <- mat
+    all.assays <- lapply(all.se, assays, withDimnames=FALSE)
+    each.assay.names <- lapply(all.assays, names)
+    no.assay.names <- vapply(each.assay.names, is.null, TRUE)
+    
+    create_dummy <- function(nr, nc) {
+        if (!delayed) {
+            array(c(nr, nc), data=NA)
+        } else {
+            ConstantArray(c(nr, nc), value=NA)
         }
-        combined[[a]] <- do.call(rbind, current)
     }
 
-    combined
+    inflate_existing <- function(mat, i) {
+        if (delayed) {
+            mat <- DelayedArray(mat)
+        }
+        if (!is.null(i)) {
+            absent <- is.na(i)
+            if (any(absent)) {
+                i[absent] <- ncol(mat)+1L
+                mat <- cbind(mat, create_dummy(nrow(mat), 1L))
+            }
+            mat <- mat[,i,drop=FALSE]
+        }
+        mat
+    }
+
+    if (any(no.assay.names)) {
+        if (!all(no.assay.names)) {
+            stop("named and unnamed assays cannot be mixed")
+        }
+        n.assays <- unique(lengths(all.assays))
+        if (length(n.assays)!=1L) {
+            stop("all SummarizedExperiments should have the same number of unnamed assays")
+        }
+        for (s in seq_along(all.se)) {
+            cur.assays <- all.assays[[s]]
+            i <- mappings[[s]]
+
+            # Filling in all missing columns.
+            for (a in seq_len(n.assays)) {
+                mat <- inflate_existing(cur.assays[[a]], i)
+                cur.assays[[a]] <- mat
+            }
+            all.assays[[s]] <- cur.assays
+        }
+    } else {
+        all.assay.names <- Reduce(union, each.assay.names)
+        for (s in seq_along(all.se)) {
+            cur.se <- all.se[[s]]
+            cur.assays <- all.assays[[s]]
+            i <- mappings[[s]]
+
+            # Filling in all missing assay names and columns.
+            for (a in all.assay.names) {
+                if (a %in% names(cur.assays)) {
+                    mat <- inflate_existing(cur.assays[[a]], i)
+                } else {
+                    if (is.null(i)) {
+                        nc <- ncol(cur.se)
+                    } else {
+                        nc <- length(i)
+                    }
+                    mat <- create_dummy(nrow(cur.se), nc)
+                }
+                cur.assays[[a]] <- mat
+            }
+            all.assays[[s]] <- cur.assays
+        }
+    }
+
+    # Re-use assay rbind'ing machinery.
+    all.assays <- lapply(all.assays, Assays)
+    combined <- do.call(rbind, all.assays)
+    as(combined, "SimpleList") 
 }
 
 setMethod("combineRows", "RangedSummarizedExperiment", function(x, y, ..., use.names=TRUE, delayed=TRUE) {
@@ -113,14 +131,16 @@ setMethod("combineRows", "RangedSummarizedExperiment", function(x, y, ..., use.n
         final.rd[[s]] <- rr
     }
 
-    # Filling in the empties. We promote everyone to a GRL if any of the individuals are GRLs.
+    # Filling in the empties. We promote everyone to a GRL if any of the
+    # individual entries are GRLs.
     has.grl <- any(vapply(final.rd, FUN=is, class2="GRangesList", FUN.VALUE=TRUE))
     for (e in which(vapply(final.rd, is.null, FUN.VALUE=TRUE))) {
         cur.se <- all.se[[e]]
         if (has.grl) {
             empty <- GRangesList(rep(list(GRanges()), nrow(cur.se)))
         } else {
-            empty <- GRanges(rep("unknown:1-0", nrow(cur.se))) # TODO: a better convention for missing intervals?
+            # TODO: a better convention for missing intervals?
+            empty <- GRanges(rep("unknown:1-0", nrow(cur.se))) 
         }
         mcols(empty) <- rowData(cur.se)
         names(empty) <- rownames(cur.se)
---
+++ inst/unitTests/test_combine-methods.R
@@ -0,0 +1,92 @@
+test_combineRows_unnamed <- function() {
+    se <- SummarizedExperiment(list(counts=matrix(rpois(1000, 10), ncol=10)))
+    colData(se)$A <- 1
+    rowData(se)$A <- 1
+
+    se2 <- SummarizedExperiment(list(counts=matrix(rpois(1000, 10), ncol=10), 
+        normalized=matrix(rnorm(1000), ncol=10)))
+    colData(se2)$B <- 2
+    rowData(se2)$B <- "B"
+
+    stuff <- combineRows(se, se2, use.names=FALSE)
+
+    # Column data is correctly combined.
+    checkIdentical(stuff$A, rep(1, ncol(stuff)))
+    checkIdentical(stuff$B, rep(2, ncol(stuff)))
+
+    # Row data is correctly combined.
+    checkIdentical(rowData(stuff)$A, rep(c(1, NA), c(nrow(se), nrow(se2))))
+    checkIdentical(rowData(stuff)$B, rep(c(NA, "B"), c(nrow(se), nrow(se2))))
+
+    # Assay data is correctly combined.
+    checkIdentical(as.matrix(assay(stuff)), rbind(assay(se), assay(se2)))
+    checkIdentical(as.matrix(assay(stuff, 2)), rbind(matrix(NA, nrow(se), ncol(se)), assay(se2, 2)))
+}
+
+test_combineRows_named <- function() {
+    se <- SummarizedExperiment(list(counts=matrix(rpois(1000, 10), ncol=10)))
+    colData(se)$A <- 1
+    rowData(se)$A <- 1
+
+    se2 <- SummarizedExperiment(list(counts=matrix(rpois(1000, 10), ncol=20), 
+        normalized=matrix(rnorm(1000), ncol=20)))
+    colData(se2)$B <- 2
+    rowData(se2)$B <- "B"
+
+    # This fails, because we expect matching numbers of columns when use.names=TRUE.
+    checkException(combineRows(se, se2), silent=TRUE)
+
+    colnames(se) <- letters[1:10]
+    colnames(se2) <- letters[3:22]
+    stuff <- combineRows(se, se2)
+
+    # Column data is correctly combined
+    checkIdentical(colnames(stuff), letters[1:22])
+    checkIdentical(stuff$A, rep(c(1, NA), c(ncol(se), 12)))
+    checkIdentical(stuff$B, rep(c(NA, 2), c(2, ncol(se2))))
+
+    # Row data is correctly combined.
+    checkIdentical(rowData(stuff)$A, rep(c(1, NA), c(nrow(se), nrow(se2))))
+    checkIdentical(rowData(stuff)$B, rep(c(NA, "B"), c(nrow(se), nrow(se2))))
+
+    # Assay data is correctly combined.
+    mat <- as.matrix(assay(stuff))
+    ref <- rbind(
+        cbind(assay(se), matrix(NA, nrow(se), ncol=12)),
+        cbind(NA, NA, assay(se2))
+    )
+    colnames(ref) <- letters[1:22]
+    checkIdentical(mat, ref)
+
+    mat <- as.matrix(assay(stuff, 2))
+    ref <- rbind(
+        matrix(NA, nrow(se), ncol(stuff)),
+        cbind(NA, NA, assay(se2, 2))
+    )
+    colnames(ref) <- letters[1:22]
+    checkIdentical(mat, ref)
+}
+
+test_combineRows_assays <- function() {
+    # Deep dive into correct assay name behavior.
+    se <- SummarizedExperiment(list(matrix(rpois(1000, 10), ncol=10)))
+    se2 <- SummarizedExperiment(list(matrix(rpois(1000, 10), ncol=10), 
+        matrix(rnorm(1000), ncol=10)))
+    colnames(se) <- letters[1:10]
+    colnames(se2) <- letters[15:24]
+    rownames(se) <- paste0("GENE_", 1:100)
+    rownames(se2) <- paste0("SPIKE_", 1:100)
+
+    # This should fail due to differences in the number of assays.
+    checkException(combineRows(se, se2), silent=TRUE)
+
+    # Either all assays are named, or all are unnamed.
+    assays(se) <- assays(se)[c(1, 1)]
+    assayNames(se2) <- c("WHEE", "BLAH")
+    checkException(combineRows(se, se2), silent=TRUE)
+
+    assays(se2) <- unname(assays(se2))
+    out <- combineRows(se, se2)
+    checkIdentical(colnames(out), letters[c(1:10, 15:24)])
+    checkIdentical(rownames(out), c(rownames(se), rownames(se2)))
+}
