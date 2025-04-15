449ce75159432604a61d6fc8f129833d1b637416
Hervé Pagès <hpages.on.github@gmail.com>
2025-04-14 23:28:56 -0700

    update NEWS

--- R/SummarizedExperiment-class.R
+++ R/SummarizedExperiment-class.R
@@ -20,6 +20,10 @@ setClass("SummarizedExperiment",
     )
 )
 
+
+setClassUnion("ExpData", c("matrix", "SummarizedExperiment"))
+
+
 ### Combine the new "parallel slots" with those of the parent class. Make
 ### sure to put the new parallel slots **first**. See R/Vector-class.R file
 ### in the S4Vectors package for what slots should or should not be considered
